f973a852b27d1014700a70b751da2f0d41573e08
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-07-05 20:22:56 -0400

    update

--- fitMultinest_binary.py
+++ fitMultinest_binary.py
@@ -1,12 +1,19 @@
 from isochrones.dartmouth import Dartmouth_Isochrone
 from isochrones.starmodel import StarModel
+from isochrones.observation import ObservationTree
 from datetime import datetime
+import pandas as pd
+
+df = pd.read_csv('df_binary.csv')
 
 dar = Dartmouth_Isochrone()
-mod = StarModel(dar)
-mod.load_hdf('mod.h5')
+t = ObservationTree.from_df(df, name='test-binary')
+t.define_models(dar, index=[0,1])
+t.add_limit(logg=(3.0,None))
+
+mod = StarModel(dar, obs=t)
 startTime = datetime.now()
-mod.fit_multinest(n_live_points=1000, refit=True)
+mod.fit_multinest(n_live_points=1000)
 time = datetime.now() - startTime
 evi = mod.evidence
 
