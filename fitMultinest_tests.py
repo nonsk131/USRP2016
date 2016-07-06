8b32b35467a90c1164fb5aef405f663b81a103d3
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-07-06 17:05:49 -0400

    update

--- fitMultinest_tests.py
+++ fitMultinest_tests.py
@@ -20,13 +20,13 @@ for n in range(0,2,1):
     t.define_models(dar)
 
     mod = StarModel(dar, obs=t)
-    mod.fit_multinest(n_live_points=1000, refit=True,
-                        basename='test{}_bound.format(i)')
+    mod.fit_multinest(n_live_points=1000,
+                        basename='test{}_bound'.format(i))
     evi = mod.evidence
     evi = str(evi)
     f1.write('case{}: '.format(i) + evi + '\n')
 
-    mod.corner_physical(props=['mass', 'distance', 'AV'])
+    mod.corner_physical(['mass_0_0','mass_0_1','distance_0','AV_0'])
     mod.corner_observed()
 
     #unassociated case
@@ -35,13 +35,13 @@ for n in range(0,2,1):
     t.define_models(dar, index = [0,1])
 
     mod = StarModel(dar, obs=t)
-    mod.fit_multinest(n_live_points=1000, refit=True,
-                        basename='test{}_unassociated.format(i)')
+    mod.fit_multinest(n_live_points=1000,
+                        basename='test{}_unassociated'.format(i))
     evi = mod.evidence
     evi = str(evi)
     f2.write('case{}: '.format(i) + evi + '\n')
 
-    mod.corner_physical(props=['mass', 'distance', 'AV'])
+    mod.corner_physical(['mass_0_0','mass_0_1','distance_0','distance_1','AV_0','AV_1'])
     mod.corner_observed()
 
 f1.close()
