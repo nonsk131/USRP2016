56d1fbaf76942f1ff9c97a99bcbf1c6f22e1abf4
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-07-06 21:01:49 -0400

    update

--- fitMultinest_tests.py
+++ fitMultinest_tests.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 f1 = open('evidence_bound.txt','w')
 f2 = open('evidence_unassociated.txt','w')
 
-for n in range(7,100,1):
+for n in range(0,100,1):
     if n < 10:
         i = '0' + str(n)
     else:
@@ -31,9 +31,11 @@ for n in range(7,100,1):
 
     #mod.corner(['mass_0_0','mass_0_1','distance_0','AV_0'])
     fig = mod.corner_physical(props=['mass', 'distance', 'AV'])
-    fig.savefig('test{}_corner_physical.png'.format(i))
+    fig.savefig('test{}_bound_corner_physical.png'.format(i))
+    plt.close(fig)
     fig = mod.corner_observed()
-    fig.savefig('test{}_corner_observed.png'.format(i))
+    fig.savefig('test{}_bound_corner_observed.png'.format(i))
+    plt.close(fig)
 
     #unassociated case
     dar = Dartmouth_Isochrone()
@@ -48,8 +50,12 @@ for n in range(7,100,1):
     f2.write('case{}: '.format(i) + evi + '\n')
 
     #mod.corner(['mass_0_0','mass_0_1','distance_0','distance_1','AV_0','AV_1'])
-    mod.corner_physical(props=['mass', 'distance', 'AV'])
-    mod.corner_observed()
+    fig = mod.corner_physical(props=['mass', 'distance', 'AV'])
+    fig.savefig('test{}_unasso_corner_physical.png'.format(i))
+    plt.close(fig)
+    fig = mod.corner_observed()
+    fig.savefig('test{}_unasso_corner_observed.png'.format(i))
+    plt.close(fig)
 
 f1.close()
 f2.close()
