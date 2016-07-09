2f1cfa4833aab4b6988c70ddb2e2498177bedbf1
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-07-09 16:35:05 -0400

    update

--- fitMultinest_tests2.py
+++ fitMultinest_tests2.py
@@ -5,12 +5,12 @@ from isochrones.starmodel import StarModel
 from isochrones.observation import ObservationTree
 import pandas as pd
 import matplotlib.pyplot as plt
-from mpi4py import MPI
+#from mpi4py import MPI
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+#comm = MPI.COMM_WORLD
+#rank = comm.Get_rank()
 
-for n in range(133,200,1):
+for n in range(100,200,1):
     if n < 10:
         i = '0' + str(n)
     else:
@@ -27,19 +27,19 @@ for n in range(133,200,1):
     mod.fit_multinest(n_live_points=1000,
                         basename='test{}_bound'.format(i))
 
-    if rank == 0:
-        f1 = open('evidence_bound2.txt','a')
-        evi = mod.evidence
-        evi = str(evi)
-        f1.write('case{}: '.format(i) + evi + '\n')
+    #if rank == 0:
+    f1 = open('evidence_bound2.txt','a')
+    evi = mod.evidence
+    evi = str(evi)
+    f1.write('case{}: '.format(i) + evi + '\n')
 
-        #mod.corner(['mass_0_0','mass_0_1','distance_0','AV_0'])
-        fig = mod.corner_physical(props=['mass', 'distance', 'AV'])
-        fig.savefig('test{}_bound_corner_physical.png'.format(i))
-        plt.close(fig)
-        fig = mod.corner_observed()
-        fig.savefig('test{}_bound_corner_observed.png'.format(i))
-        plt.close(fig)
+    #mod.corner(['mass_0_0','mass_0_1','distance_0','AV_0'])
+    fig = mod.corner_physical(props=['mass', 'distance', 'AV'])
+    fig.savefig('test{}_bound_corner_physical.png'.format(i))
+    plt.close(fig)
+    fig = mod.corner_observed()
+    fig.savefig('test{}_bound_corner_observed.png'.format(i))
+    plt.close(fig)
 
     #unassociated case
     dar = Dartmouth_Isochrone()
@@ -50,17 +50,17 @@ for n in range(133,200,1):
     mod.fit_multinest(n_live_points=1000,
                         basename='test{}_unassociated'.format(i))
 
-    if rank == 0:
-        f2 = open('evidence_unassociated2.txt','a')
-        evi = mod.evidence
-        evi = str(evi)
-        f2.write('case{}: '.format(i) + evi + '\n')
-        f2.close()
+    #if rank == 0:
+    f2 = open('evidence_unassociated2.txt','a')
+    evi = mod.evidence
+    evi = str(evi)
+    f2.write('case{}: '.format(i) + evi + '\n')
+    f2.close()
 
-        #mod.corner(['mass_0_0','mass_0_1','distance_0','distance_1','AV_0','AV_1'])
-        fig = mod.corner_physical(props=['mass', 'distance', 'AV'])
-        fig.savefig('test{}_unasso_corner_physical.png'.format(i))
-        plt.close(fig)
-        fig = mod.corner_observed()
-        fig.savefig('test{}_unasso_corner_observed.png'.format(i))
-        plt.close(fig)
+    #mod.corner(['mass_0_0','mass_0_1','distance_0','distance_1','AV_0','AV_1'])
+    fig = mod.corner_physical(props=['mass', 'distance', 'AV'])
+    fig.savefig('test{}_unasso_corner_physical.png'.format(i))
+    plt.close(fig)
+    fig = mod.corner_observed()
+    fig.savefig('test{}_unasso_corner_observed.png'.format(i))
+    plt.close(fig)
