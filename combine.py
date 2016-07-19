e81a82b5d3ae39e0189417f42aa89574a5600013
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-07-19 05:53:40 -0400

    update

--- combine.py
+++ combine.py
@@ -3,5 +3,5 @@ import pandas as pd
 df1 = pd.read_csv('/tigress/np5/all_df.csv')
 df2 = pd.read_csv('/tigress/np5/all_df_params.csv')
 
-df = pd.concat([df1,df2], axis=1)
+df = pd.merge(df1, df2, left_index=True, right_index=True, how='inner')
 df.to_csv(path_or_buf='/tigress/np5/all_df_final.csv')
