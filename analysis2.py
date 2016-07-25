edbd26647227794a70fe3f4cf9dd9b0f20a3d58c
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-07-25 17:17:32 -0400

    update

--- analysis2.py
+++ analysis2.py
@@ -29,35 +29,41 @@ for line in file:
             M2 = float(line[1])
             age1 = float(line[2])
             age2 = float(line[3])
+            feh1 = float(line[4])
+            feh2 = float(line[5])
             distance1 = float(line[6])
             distance2 = float(line[7])
+            AV1 = float(line[8])
+            AV2 = float(line[9][:-1])
             df1 = df1.append(pd.DataFrame({'M1':M1,
                                     'M2':M2,
                                     'age1':age1,
                                     'age2':age2,
-                                    'feh1':0.0,
-                                    'feh2':0.2,
+                                    'feh1':feh1,
+                                    'feh2':feh2,
                                     'distance1':distance1,
                                     'distance2':distance2,
-                                    'AV1':0.0,
-                                    'AV2':0.1},index=[index]))
+                                    'AV1':AV1,
+                                    'AV2':AV2},index=[index]))
         elif tracker == 'bound':
             line = line.split('=')[1]
             line = line.split(',')
             M1 = float(line[0][2:])
             M2 = float(line[1])
             age1 = float(line[2])
+            feh1 = float(line[3])
             distance1 = float(line[4])
+            AV1 = float(line[5][:-1])
             df2 = df2.append(pd.DataFrame({'M1':M1,
                                     'M2':M2,
                                     'age1':age1,
                                     'age2':age1,
-                                    'feh1':0.0,
-                                    'feh2':0.0,
+                                    'feh1':feh1,
+                                    'feh2':feh1,
                                     'distance1':distance1,
                                     'distance2':distance1,
-                                    'AV1':0.0,
-                                    'AV2':0.0},index=[index]))
+                                    'AV1':AV1,
+                                    'AV2':AV1},index=[index]))
         else:
             pass
             # line = line.split('=')[1]
