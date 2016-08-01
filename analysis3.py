985691957805812880bfa2152567aab3a2259991
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-08-01 19:32:28 -0400

    update

--- analysis3.py
+++ analysis3.py
@@ -4,6 +4,8 @@ from isochrones.starmodel import StarModel
 from isochrones.observation import ObservationTree
 import numpy as np
 
+df = pd.DataFrame()
+
 i = 6000
 df = pd.read_csv('/tigress/np5/dataFrame/df_binary_test{}.csv'.format(i))
 
@@ -16,7 +18,15 @@ mod = StarModel(dar, obs=t)
 mod.fit_multinest(n_live_points=1000,
                     basename='/tigress/np5/chains/test{}_unassociated'.format(i))
 
-print mod.samples.mass_0_0.median()
-print mod.samples.mass_1_0.median()
-print mod.samples.distance_0.median()
-print mod.samples.distance_1.median()
+M1 = mod.samples.mass_0_0.median()
+M2 = mod.samples.mass_1_0.median()
+distance1 = mod.samples.distance_0.median()
+distance2 = mod.samples.distance_1.median()
+
+index = 'case'+str(i)
+df = df.append({'M1':M1,
+                'M2':M2,
+                'distance1':distance1,
+                'distance2':distance2},index=[index])
+
+print df
