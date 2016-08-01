da2a9da0aa1d3c95185aaf677babdab83b2149cd
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-08-01 19:42:28 -0400

    update

--- analysis3.py
+++ analysis3.py
@@ -6,27 +6,27 @@ import numpy as np
 
 df_data = pd.DataFrame()
 
-i = 6000
-df = pd.read_csv('/tigress/np5/dataFrame/df_binary_test{}.csv'.format(i))
+for i in range(6000,7000,1):
+    df = pd.read_csv('/tigress/np5/dataFrame/df_binary_test{}.csv'.format(i))
 
-dar = Dartmouth_Isochrone()
-t = ObservationTree.from_df(df, name='test{}'.format(i))
+    dar = Dartmouth_Isochrone()
+    t = ObservationTree.from_df(df, name='test{}'.format(i))
 
-t.define_models(dar, index=[0,1])
+    t.define_models(dar, index=[0,1])
 
-mod = StarModel(dar, obs=t)
-mod.fit_multinest(n_live_points=1000,
-                    basename='/tigress/np5/chains/test{}_unassociated'.format(i))
+    mod = StarModel(dar, obs=t)
+    mod.fit_multinest(n_live_points=1000,
+                        basename='/tigress/np5/chains/test{}_unassociated'.format(i))
 
-M1 = mod.samples.mass_0_0.median()
-M2 = mod.samples.mass_1_0.median()
-distance1 = mod.samples.distance_0.median()
-distance2 = mod.samples.distance_1.median()
+    M1 = mod.samples.mass_0_0.median()
+    M2 = mod.samples.mass_1_0.median()
+    distance1 = mod.samples.distance_0.median()
+    distance2 = mod.samples.distance_1.median()
 
-index = 'case'+str(i)
-df_data = df_data.append(pd.DataFrame({'M1':M1,
-                'M2':M2,
-                'distance1':distance1,
-                'distance2':distance2},index=[index]))
+    index = 'case'+str(i)
+    df_data = df_data.append(pd.DataFrame({'M1_fit':M1,
+                    'M2_fit':M2,
+                    'distance1_fit':distance1,
+                    'distance2_fit':distance2},index=[index]))
 
-print df_data
+df_data.to_csv(path_or_buf='/tigress/np5/df_fit_params.csv')
