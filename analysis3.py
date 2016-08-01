3b8dcf9323010807b33457cf9eec4591c9cf23ac
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-08-01 19:14:49 -0400

    analysis3

---
+++ analysis3.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from isochrones.dartmouth import Dartmouth_Isochrone
+from isochrones.starmodel import StarModel
+from isochrones.observation import ObservationTree
+import numpy as np
+
+i = 6000
+df = pd.read_csv('/tigress/np5/dataFrame/df_binary_test{}.csv'.format(i))
+
+dar = Dartmouth_Isochrone()
+t = ObservationTree.from_df(df, name='test{}'.format(i))
+
+t.define_models(dar)
+
+mod = StarModel(dar, obs=t)
+mod.fit_multinest(n_live_points=1000,
+                    basename='/tigress/np5/chains/test{}_bound'.format(i))
+
+print mod.samples.mass_0_0.median()
