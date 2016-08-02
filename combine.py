7ae6d05e5a6b23fe2233fc38b6aced264c464502
nonsk131 <nondh@nat-oitwireless-inside-vapornet100-b-10140.Princeton.EDU>
2016-08-01 21:37:17 -0400

    update

--- combine.py
+++ combine.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 df1 = pd.read_csv('/tigress/np5/all_df.csv', index_col=0)
 df2 = pd.read_csv('/tigress/np5/all_df_params.csv', index_col=0)
+df3 = pd.read_csv('/tigress/np5/df_fit_params.csv', index_col=0)
 
 df = pd.merge(df1, df2, left_index=True, right_index=True, how='inner')
+df = pd.merge(df, df3, left_index=True, right_index=True, how='inner')
 df.to_csv(path_or_buf='/tigress/np5/all_df_final.csv')
